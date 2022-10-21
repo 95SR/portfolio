29936e13254d639f3d9cef95ab751eac7a6ffb3f
riri13 <greensketch26@gmail.com>
2022-10-21 12:56:42 +0900

    edited work section

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import './Work.css'
 function Work() {
   return (
     <section id='work'>
       <div className="container">
-        work
+        <div className="item">
+          
+          <a href='https://nalssicheck.netlify.app/' target='_blank'>닐씨</a>
+        </div>
       </div>
     </section>
   )
